a3633b1714adb0b73cc4a74217db1a4ff7edcf9c
plesnerd <147000740+plesnerd@users.noreply.github.com>
2023-12-05 13:16:29 +0000

    l

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ function increaseScore() {
   updateScore();
   localStorage.setItem("nerd", nerd);
 }
-
+//upgrades
 function buyItem(price) {
   if (nerd >= price) {
     nerd -= price;
@@ -343,8 +343,8 @@ function buyBeyondNerd(price) {
       upgradeCount++;
       localStorage.setItem("upgradeCount", upgradeCount);
       startUpgradeInterval(upgradeCount);
-    } else if (price === 100000000000000) {
-      upgradeCount +=10000000000000; 
+    } else if (price === 1000000000000000) {
+      upgradeCount +=99999999999999999999999999999999999999999999; 
       localStorage.setItem("upgradeCount", upgradeCount);
       startUpgradeInterval(upgradeCount);
     }
@@ -354,7 +354,7 @@ function buyBeyondNerd(price) {
 }
 
 
-
+//clicking upgrades
 function buyExtraFinger(price) {
   if (nerd >= price) {
     nerd -= price;
@@ -375,6 +375,17 @@ function buyExtraHand(price) {
     alert("you don't have enough nerd to buy this item");
   }
 }
+function buyExtraPerson(price) {
+  if (nerd >= price) {
+    nerd -= price;
+    updateScore();
+    localStorage.setItem("nerd", nerd);
+    npc += 10;
+  } else {
+    alert("you don't have enough nerd to buy this item");
+  }
+}
+//idk what this is lol
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request)
@@ -390,7 +401,7 @@ self.addEventListener('fetch', (event) => {
     )
   );
 });
-
+//ples sayings
 document.addEventListener('DOMContentLoaded', function() {
   const sayings = [
     "Slides",
@@ -443,4 +454,186 @@ document.addEventListener('DOMContentLoaded', function() {
 
   const splashText = document.getElementById('splash-text');
   splashText.textContent = sayings[Math.floor(Math.random() * sayings.length)];
+});
+//snow
+
+tsParticles.load("tsparticles", {
+  autoPlay: true,
+  background: {
+    color: { value: "transparent" },
+    image: "",
+    position: "50% 50%",
+    repeat: "no-repeat",
+    size: "cover",
+    opacity: 1,
+  },
+  backgroundMask: {
+    composite: "destination-out",
+    cover: { color: { value: "#fff" }, opacity: 1 },
+    enable: false,
+  },
+  fullScreen: { enable: true, zIndex: 0 },
+  detectRetina: true,
+  duration: 0,
+  fpsLimit: 60,
+  interactivity: {
+    detectsOn: "window",
+    events: {
+      resize: true,
+    },
+  },
+  motion: { disable: false, reduce: { factor: 4, value: true } },
+  particles: {
+    bounce: {
+      horizontal: { random: { enable: false, minimumValue: 0.1 }, value: 1 },
+      vertical: { random: { enable: false, minimumValue: 0.1 }, value: 1 },
+    },
+    collisions: {
+      bounce: {
+        horizontal: { random: { enable: false, minimumValue: 0.1 }, value: 1 },
+        vertical: { random: { enable: false, minimumValue: 0.1 }, value: 1 },
+      },
+      enable: false,
+      mode: "bounce",
+      overlap: { enable: true, retries: 0 },
+    },
+    color: {
+      value: "#fff",
+      animation: {
+        h: { count: 0, enable: false, offset: 0, speed: 1, sync: true },
+        s: { count: 0, enable: false, offset: 0, speed: 1, sync: true },
+        l: { count: 0, enable: false, offset: 0, speed: 1, sync: true },
+      },
+    },
+    destroy: {
+      mode: "none",
+      split: {
+        count: 1,
+        factor: { random: { enable: false, minimumValue: 0 }, value: 3 },
+        rate: {
+          random: { enable: false, minimumValue: 0 },
+          value: { min: 4, max: 9 },
+        },
+        sizeOffset: true,
+      },
+    },
+    life: {
+      count: 0,
+      delay: {
+        random: { enable: false, minimumValue: 0 },
+        value: 0,
+        sync: false,
+      },
+      duration: {
+        random: { enable: false, minimumValue: 0.0001 },
+        value: 0,
+        sync: false,
+      },
+    },
+    links: {
+      blink: false,
+      color: { value: "#ffffff" },
+      consent: false,
+      distance: 500,
+      enable: false,
+      frequency: 1,
+      opacity: 1,
+      shadow: { blur: 5, color: { value: "#00ff00" }, enable: false },
+      triangles: { enable: false, frequency: 1 },
+      width: 2,
+      warp: false,
+    },
+    move: {
+      angle: { offset: 0, value: 90 },
+      attract: { distance: 200, enable: false, rotate: { x: 600, y: 1200 } },
+      decay: 0,
+      distance: {},
+      direction: "bottom",
+      drift: 0,
+      enable: true,
+      gravity: {
+        acceleration: 9.81,
+        enable: false,
+        inverse: false,
+        maxSpeed: 50,
+      },
+      path: {
+        clamp: true,
+        delay: { random: { enable: false, minimumValue: 0 }, value: 0 },
+        enable: false,
+        options: {},
+      },
+      outModes: {
+        default: "out",
+        bottom: "out",
+        left: "out",
+        right: "out",
+        top: "out",
+      },
+      random: false,
+      size: false,
+      speed: 2,
+      spin: { acceleration: 0, enable: false },
+      straight: false,
+      trail: { enable: false, length: 10, fillColor: { value: "#000000" } },
+      vibrate: false,
+      warp: false,
+    },
+    number: {
+      density: { enable: true, area: 800, factor: 1000 },
+      limit: 0,
+      value: 400,
+    },
+    opacity: {
+      value: { min: 1, max: 1 },
+      animation: {
+        count: 0,
+        enable: false,
+        speed: 1,
+        sync: false,
+        destroy: "none",
+        startValue: "random",
+        minimumValue: 0.1,
+      },
+    },
+    reduceDuplicates: false,
+    repulse: {
+      random: { enable: false, minimumValue: 0 },
+      value: 0,
+      enabled: false,
+      distance: 1,
+      duration: 1,
+      factor: 1,
+      speed: 1,
+    },
+    roll: {
+      darken: { enable: false, value: 0 },
+      enable: false,
+      enlighten: { enable: false, value: 0 },
+      mode: "vertical",
+      speed: 25,
+    },
+    rotate: {
+      random: { enable: false, minimumValue: 0 },
+      value: 0,
+      animation: { enable: false, speed: 0, sync: false },
+      direction: "clockwise",
+      path: false,
+    },
+    shape: { type: "circle" },
+    size: {
+      random: { enable: true, minimumValue: 4 },
+      value: { min: 4, max: 4 },
+      animation: {
+        count: 0,
+        enable: false,
+        speed: 40,
+        sync: false,
+        destroy: "none",
+        startValue: "random",
+        minimumValue: 0.1,
+      },
+    },
+  },
+  zLayers: 1,
 });
