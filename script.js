e62c395e45704d00266186f031abce5efb23a26b
Bluebay Studios <147000740+plesnerd@users.noreply.github.com>
2024-10-17 15:07:12 +0000

    Fix particle

--- script.js
+++ script.js
@@ -372,8 +372,42 @@ function buyIslandNerd(price) {
       localStorage.setItem("upgradeCount", upgradeCount);
       startUpgradeInterval(upgradeCount);
 
+      const defaults = {
+          spread: 360,
+          ticks: 100,
+          gravity: 0,
+          decay: 0.94,
+          startVelocity: 30,
+        };
 
-    }
+        function shoot() {
+          confetti({
+            ...defaults,
+            particleCount: 30,
+            scalar: 1.2,
+            shapes: ["emoji"],
+            colors: ["#a864fd", "#29cdff", "#78ff44", "#ff718d", "#fdff6a"],
+          });
+
+          confetti({
+            ...defaults,
+            particleCount: 20,
+            scalar: 2,
+            shapes: ["emoji"],
+            shapeOptions: {
+              emoji: {
+                value: "🍁",
+              },
+            },
+          });
+        }
+
+       setTimeout(shoot, 0);
+        setTimeout(shoot, 100);
+        setTimeout(shoot, 200);
+
+      }
+    
   } else {
     alert("you don't have enough nerd to buy this item");
   }
