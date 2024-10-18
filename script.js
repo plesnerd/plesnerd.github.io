079a89217ebadda00d033bd28a9afa9aea294b31
Bluebay Studios <147000740+plesnerd@users.noreply.github.com>
2024-10-18 03:06:42 +0000

    What am I doin

--- script.js
+++ script.js
@@ -396,40 +396,39 @@ function buyYachtNerd(price) {
       startUpgradeInterval(upgradeCount);
       updateUpgradeCount();
 
-      const defaults = {
-        spread: 360,
-        ticks: 100,
-        gravity: 0,
-        decay: 0.94,
-        startVelocity: 30,
-      };
+    const defaults = {
+      spread: 360,
+      ticks: 100,
+      gravity: 0,
+      decay: 0.94,
+      startVelocity: 30,
+    };
 
-      function shoot() {
-        confetti({
-          ...defaults,
-          particleCount: 30,
-          scalar: 1.2,
-          shapes: ["emoji"],
-          colors: ["#a864fd", "#29cdff", "#78ff44", "#ff718d", "#fdff6a"],
-        });
+    function shoot() {
+      confetti({
+        ...defaults,
+        particleCount: 30,
+        scalar: 1.2,
+        shapes: ["emoji"],
+        colors: ["#a864fd", "#29cdff", "#78ff44", "#ff718d", "#fdff6a"],
+      });
 
-        confetti({
-          ...defaults,
-          particleCount: 20,
-          scalar: 2,
-          shapes: ["emoji"],
-          shapeOptions: {
-            emoji: {
-              value: "🍁",
-            },
+      confetti({
+        ...defaults,
+        particleCount: 20,
+        scalar: 2,
+        shapes: ["emoji"],
+        shapeOptions: {
+          emoji: {
+            value: "🍁",
           },
-        });
-      }
+        },
+      });
+    }
 
-     setTimeout(shoot, 0);
-      setTimeout(shoot, 100);
-      setTimeout(shoot, 200);
-
+    setTimeout(shoot, 0);
+    setTimeout(shoot, 100);
+    setTimeout(shoot, 200);
     }
   } else {
     alert("you don't have enough nerd to buy this item");
